8419fb85203c90a638fb036a809e7d5125868279
Lloyd Francis <lloydaf@gmail.com>
2021-10-03 13:50:21 +0200

    improvement: adding some random colors for page, getting the switch functionality working

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,22 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-import { Card, Box, Text, Checkbox } from '@freenow/wave'
+import { Card, Box, Text, Checkbox, Button } from '@freenow/wave'
 
 import { Page as PageType } from '../types'
 
-export const Page = ({ page }: { page: PageType }): JSX.Element => (
-  <Card>
-    <Box>
-      <Text>Attribute</Text>
-      <Text>Most</Text>
-      <Text>Least</Text>
-    </Box>
-    {page.fields.map((field) => (
-      <Box key={field.text}>
-        <Text>{field.text}</Text>
-        <Checkbox value={field.most} />
-        <Checkbox value={field.least} />
+export const Page = ({ page }: { page: PageType }): JSX.Element => {
+  const [least, setLeast] = useState<{ index: number, value: string }>()
+  const [most, setMost] = useState<{ index: number, value: string }>()
+
+  const clear = () => {
+    setLeast(undefined)
+    setMost(undefined)
+  }
+  return (
+    <Card level="100">
+      <Box id="heading" >
+        <Box display="flex" justifyContent="space-evenly" background="red">
+          <Text>Attribute</Text>
+          <Text>Most</Text>
+          <Text>Least</Text>
+        </Box>
       </Box>
-    ))}
-  </Card>
-)
+      <Box id="body">
+        {page.fields.map((field, index) => (
+          <Box key={field.text} display="flex" justifyContent="space-evenly" background="green">
+            <Text>{field.text}</Text>
+            <Checkbox value={field.most} disabled={most?.index !== undefined && most.index !== index} onChange={e => e.target.checked ? setMost({ index, value: e.target.value }) : setMost(undefined)} checked={most?.index === index} />
+            <Checkbox value={field.least} disabled={least?.index !== undefined && least.index !== index} onChange={e => e.target.checked ? setLeast({ index, value: e.target.value }) : setLeast(undefined)} checked={least?.index === index} />
+          </Box>
+        ))}
+      </Box>
+      <Box id="footer">
+        <Button onClick={clear}>Clear</Button>
+        <Button>Continue</Button>
+      </Box>
+    </Card>
+  )
+}
